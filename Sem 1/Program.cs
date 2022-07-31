6102971454e05fb46d710e17d34571b415f7bfa3
boris.s <boris.sidorov.28@gmail.com>
2022-07-31 20:14:49 +0300

    Добавлена функция возведения числа во вторую степень

---
+++ Sem 1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число для возведения во вторую стпень");
+
+int num = Convert.ToInt32(Console.ReadLine());
+int rezult = power(num);
+Console.WriteLine(rezult);
+
+int power(int currentNumber)
+{
+    return currentNumber * currentNumber;
+}
